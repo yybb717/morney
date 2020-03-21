7f9e1486902189353320a6b4c72905e377d7dc7e
yybb717 <2638143715@qq.com>
2020-03-21 21:07:21 +0800

    提交

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -8,6 +8,8 @@ type TagListModel = {
   fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated'; // 联合类型
   save: () => void;
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  remove: (id: string) => boolean;
 }
 const tagListModel: TagListModel = {
   //声明标签列表data
@@ -22,13 +24,43 @@ const tagListModel: TagListModel = {
     // this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
     const names = this.data.map(item => item.name);
     if (names.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push({id:name, name: name});
+    this.data.push({id: name, name: name});
     this.save();
     return 'success';
   },
   //把标签列表保存到数据库里
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
-  }
-};
+  },
+    update(id, name) {
+      const idList = this.data.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = this.data.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          return 'duplicated';
+        } else {
+          const tag = this.data.filter(item => item.id === id)[0];
+          tag.name = name;
+          this.save();
+          return 'success';
+        }
+      } else {
+        return 'not found';
+      }
+    },
+    remove(id: string) {
+      let index = -1;
+      for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      this.data.splice(index, 1);
+      this.save();
+      return true;
+    },
+
+}
+;
 export {tagListModel};
